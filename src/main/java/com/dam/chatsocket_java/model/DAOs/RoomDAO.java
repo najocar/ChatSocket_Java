3fb956267c01ff181f292a85362b423355fa08a7
Ruben <rubenca2312@gmail.com>
2023-09-27 22:22:28 +0200

    msgDAO created and schema in file, fix msg class (user)

---
+++ src/main/java/com/dam/chatsocket_java/model/DAOs/RoomDAO.java
@@ -0,0 +1,10 @@
+package com.dam.chatsocket_java.model.DAOs;
+
+public class RoomDAO {
+    // room[].xml -> {
+    //      roomId: string,
+    //      msgs -> {
+    //          msg: Msg
+    //      }
+    //  }
+}
